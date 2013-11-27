10c5dfc0cd22b716f9360fafb0b212ff52aee603
Dan Cameron <dan@sproutventure.com>
2013-11-27 12:25:44 -0800

    New Summary based on prices that could be changed.

--- groupBuyingReports.class.php
+++ groupBuyingReports.class.php
@@ -74,8 +74,8 @@ class Group_Buying_Reports_SS extends Group_Buying_Controller {
 		}
 		$gb_report_pages = count( $merchants_deal_ids )/10; // set the global for later pagination
 
-		$start_time = ( isset( $_REQUEST['summary_sales_start_date'] ) && strtotime( $_REQUEST['summary_sales_start_date'] ) <= current_time( 'timestamp' ) ) ? strtotime( $_REQUEST['summary_sales_start_date'] ) : current_time( 'timestamp' )-604800;
-  		$time = ( isset( $_REQUEST['summary_sales_end_date'] ) && strtotime( $_REQUEST['summary_sales_end_date'] ) <= current_time( 'timestamp' ) ) ? strtotime( $_REQUEST['summary_sales_end_date'] ) : current_time( 'timestamp' );
+		$start_time = ( isset( $_REQUEST['summary_sales_start_date'] ) && strtotime( $_REQUEST['summary_sales_start_date'] ) <= current_time( 'timestamp' ) ) ? $_REQUEST['summary_sales_start_date'] : date( 'm/d/Y', current_time( 'timestamp' )-604800 );
+  		$time = ( isset( $_REQUEST['summary_sales_end_date'] ) && strtotime( $_REQUEST['summary_sales_end_date'] ) <= current_time( 'timestamp' ) ) ? $_REQUEST['summary_sales_end_date'] : date( 'm/d/Y', current_time( 'timestamp' ) );
 
 		$credit_payment_methods = apply_filters( 'set_credit_purchases_methods', array( Group_Buying_Affiliate_Credit_Payments::PAYMENT_METHOD, Group_Buying_Account_Balance_Payments::PAYMENT_METHOD ) );
 
@@ -84,6 +84,7 @@ class Group_Buying_Reports_SS extends Group_Buying_Controller {
 			// loop through all the deals
 			foreach ( $merchants_deal_ids as $deal_id ) {
 				$deal = Group_Buying_Deal::get_instance( $deal_id );
+				$deal_title = $deal->get_title();
 
 				$args=array(
 					'post_type' => Group_Buying_Purchase::POST_TYPE,
@@ -91,50 +92,121 @@ class Group_Buying_Reports_SS extends Group_Buying_Controller {
 					'fields' => 'ids',
 					'date_query' => array(
 							array(
-								'after'     => $_REQUEST['summary_sales_start_date'],
-								'before'    => $_REQUEST['summary_sales_end_date'],
+								'after'     => $start_time,
+								'before'    => date( 'm/d/Y', strtotime( $time )+86400 ), // Add a day since it will nto count the date selected otherwise.
 								'inclusive' => true,
 							),
 						),
+					'meta_query' => array(
+							array(
+								'key'     => '_deal_id',
+								'value'    => $deal_id,
+							),
+						),
 				);
+
 				$purchases = new WP_Query( $args );
 				
-				// Loop through all the purchases to build variables.
-				$qty = 0;
-				$credits = 0;
+				// Loop through all the purchases to build variables, with a key of the price.
+				$prices = array();
 				foreach ( $purchases->posts as $purchase_id ) {
 					$purchase = Group_Buying_Purchase::get_instance( $purchase_id );
-					$purchase_items = $purchase->get_products();
-					foreach ( $purchase_items as $key => $item ) {
-						if ( $deal_id == $item['deal_id'] ) {
-							$qty += $item['quantity'];
-						}
-					}
 					$payment_ids = $purchase->get_payments();
+					// Loop through all payments for purchase
 					foreach ( $payment_ids as $payment_id ) {
 						$payment = Group_Buying_Payment::get_instance( $payment_id );
-						if ( in_array( $payment->get_payment_method(), $credit_payment_methods ) ) {
-							$payment_items = $payment->get_deals();
-							foreach ( $payment_items as $key_item_id => $payment_data ) {
+						$payment_items = $payment->get_deals();
+						//error_log( 'payment items: ' . print_r( $payment_items, TRUE ) );
+						foreach ( $payment_items as $key_item_id => $payment_key ) {
+							/*/
+								Example: 
+								(
+								    [0] => Array
+								        (
+								            [deal_id] => 499
+								            [quantity] => 5
+								            [data] => Array
+								                (
+								                )
+
+								            [price] => 200
+								            [unit_price] => 40
+								            [payment_method] => Array
+								                (
+								                    [Account Credit (Affiliate)] => 200
+								                )
+
+								        )
+
+								)
+							/**/
+							foreach ( $payment_key as $payment_data ) {
+								// Info only useful if the data is associated with the deal_id
 								if ( $deal_id == $key_item_id ) {
-									foreach ( $payment_data[0]['payment_method'] as $payment_method => $total ) {
-										if ( in_array( $payment->get_payment_method(), $credit_payment_methods ) ) {
-											$credits += $total;
+									// Make sure an array is set for this price
+									if ( !array_key_exists( $payment_data['unit_price'], $prices ) ) {
+										$prices[$payment_data['unit_price']] = array();
+									}
+
+									// Set the quantity purchased
+									$prices[$payment_data['unit_price']]['qty'] += $payment_data['quantity'];
+
+									// Credits
+									if ( !isset( $prices[$payment_data['unit_price']]['credits'] ) ) {
+										$prices[$payment_data['unit_price']]['credits'] = 0;
+									}
+
+									// Loop through each payment method to calculate credits used.
+									foreach ( $payment_data['payment_method'] as $payment_method => $total ) {
+										if ( in_array( $payment_method, $credit_payment_methods ) ) {
+											$prices[$payment_data['unit_price']]['credits'] += $total;
 										}
 									}
-								}
+								}	
 							}
 						}
 					}
 				}
-				// Records are based on the deal and the results of all of it's purchases.
-				$purchase_array[] = apply_filters( 'gb_sales_summary_record_item', array(
-						'deal_name' => $deal->get_title(),
-						'qty' => $qty,
-						'price' => gb_get_formatted_money( $deal->get_price() ),
-						'credits' => $credits,
-						'earn' => ( $qty * $deal->get_price() ) - $credits
-					), $deal );
+
+				/**
+				 * Setup a parent record if the deal has had multiple prices.
+				 */
+				$multiple = ( count( $prices ) > 1 ) ? TRUE : FALSE ;
+				if ( $multiple ) {
+					// Get totals
+					foreach ( $prices as $price => $data ) {
+						$total_qty += $data['qty'];
+						$total_credits += $data['credits'];
+						$total_price += $price;
+						$total_price_qty += $price*$data['qty'];
+					}
+					$average_price = ($total_price_qty/$total_qty);
+					// Records are based on the deal and the results of all of it's purchases.
+					$purchase_array[] = apply_filters( 'gb_sales_summary_record_item', array(
+							'deal_name' => $deal_title,
+							'qty' => $total_qty,
+							'price' => gb_get_formatted_money( $average_price ),
+							'credits' => $total_credits,
+							'earn' => ( $total_qty * $average_price ) - $total_credits
+						), $deal );
+				}
+
+				// Modify the deal title if these next items are sub-items
+				if ( $multiple ) {
+					$deal_title = ' &rsaquo; <em>' . $deal_title . '</em>';
+				}
+
+				// Loop through each price and make an entry.
+				foreach ( $prices as $price => $data ) {
+					// Records are based on the deal and the results of all of it's purchases.
+					$purchase_array[] = apply_filters( 'gb_sales_summary_record_item', array(
+							'deal_name' => $deal_title,
+							'qty' => $prices[$price]['qty'],
+							'price' => gb_get_formatted_money( $price ),
+							'credits' => $prices[$price]['credits'],
+							'earn' => ( $prices[$price]['qty'] * $price ) - $prices[$price]['credits']
+						), $deal );
+				}
 			}
 		}
 		$report->records = apply_filters( 'set_sales_summary_report_records', $purchase_array );
